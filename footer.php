25d0f3f4f3491fa915345b9a566db67d08a1ed0b
goup73 <cinthialeco@gmail.com>
2020-11-16 17:18:39 +0100

    ajout du footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+	<p>clairehermet@hotmail.com</p>
+	<a href="https://www.faceup-facialiste.fr/mentionlegale/" id="mentionL">Mentions légales</a>
+	<a href="https://www.instagram.com/faceup_facialist/"><img src="images/instagram.png" id="insta"/></a>
+	<a href="https://www.instagram.com/prendrerendezvous/" id="BoutonRdv">Prendre rendez-vous</a>
+</footer>
