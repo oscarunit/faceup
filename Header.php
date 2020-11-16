e708d77e750971833b71d1afa53df5220d06855e
Clotilde74 <clotildelarfaillou@gmail.com>
2020-11-16 17:16:19 +0100

    Ajout Header

---
+++ Header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Clotilde LARFAILLOU">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style/normalize.css">
+    <title>FaceUp</title>
+</head>
+<body>
+  <header>
+    <img class="logo" src="" alt="logo FaceUp">
+    <nav>
+        <a href="">Notre méthode FaceUp</a>
+        <a href="">Votre soin</a>
+        <a href="">Formation</a>
+        <a href="">Prendre rendez-vous</a>
+        <a href="">E-shop</a>
+      </nav>
+    </header>
+    <main>
+      <div>
+        <h1> Sculptez votre peau avec une méthode 100% naturelle </h1>
+        <input class="favorite styled" type="button" value="Découvrir">
+      </div>
+      <div class="points">
+                <span></span>
+                <span></span>
+                <span></span>
+        </div>
+    </main>
+</boby>
+</html>
