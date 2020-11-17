b967e75ede772fd3242050bf9efc5e9fff0c80fe
oscarunit <mateo.pernet@outlook.fr>
2020-11-17 21:00:57 +0100

    header et functions

--- Header.php
+++ Header.php
@@ -1,29 +1,25 @@
-  <!DOCTYPE html>
-  <html lang="fr">
+    <!DOCTYPE html>
+    <html lang="fr">
 
-  <head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta name="author" content="Agence Bzzz">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1.0">
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <title><?php bloginfo('name');?></title>
-  <link rel="profile" href="http://gmpg.org/xfn/11" />
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-  
-  <?php wp_head(); ?>
-      <title><?php wp_title();?></title>
-  </head>
-  <body>
-    <header>
-      <img class="logo" src="" alt="logo FaceUp">
-      <nav>
-          <a href="">Notre méthode FaceUp</a>
-          <a href="">Votre soin</a>
-          <a href="">Formation</a>
-          <a href="">Prendre rendez-vous</a>
-          <a href="">E-shop</a>
-        </nav>
+      <meta charset="utf-8">
+      <meta name="author" content="Agence Bzzz">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1.0">
+      <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <title><?php bloginfo('name');?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+
+    <?php wp_head(); ?>
+        <title><?php wp_title();?></title>
+    </head>
+    <body>
+      <header>
+        <a href="<?php echo home_url( '/' ); ?>">
+              <img src="<?php echo get_template_directory_uri(); ?>/image/faceup.svg" alt="Logo FaceUp - Retour à l'accueil">
+        </a>
+        <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
       </header>
--- functions.php
+++ functions.php
@@ -6,3 +6,7 @@ function faceup_theme() {
     wp_enqueue_style('footer_style', get_theme_file_uri('css/footer.css'));
   }
 add_action( 'wp_enqueue_scripts', 'faceup_theme' );
+
+register_nav_menus( array(
+	'main_menu' => 'Menu Principal',
+) );
