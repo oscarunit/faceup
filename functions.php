56f924b3592e4349afc32369b3ce992d6c562ce9
oscarunit <mateo.pernet@outlook.fr>
2020-11-17 13:41:54 +0100

    style edited function.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 // Déclarer style.css à la racine du thème
 wp_enqueue_style(
-    'capitaine',
+    'theme_faceup',
     get_stylesheet_uri(),
     array(),
     '1.0'
@@ -10,10 +10,8 @@ wp_enqueue_style(
 
 // Déclarer un autre fichier CSS
 wp_enqueue_style(
-    'capitaine',
+    'theme_faceup',
     get_template_directory_uri() . '/css/header.css',
     array(),
     '1.0'
 );
-
-?>
