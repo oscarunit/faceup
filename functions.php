1b84e9848ee11bd8699add617093531c9ac13490
oscarunit <mateo.pernet@outlook.fr>
2020-11-17 14:30:15 +0100

    modifications

--- functions.php
+++ functions.php
@@ -1,22 +1,6 @@
 <?php
 
-function faceup_register_assets(){
-
-// Déclarer style.css à la racine du thème
-wp_enqueue_style(
-    'theme_faceup',
-    get_stylesheet_uri(),
-    array(),
-    '1.0'
-);
-
-// Déclarer un autre fichier CSS
-wp_enqueue_style(
-    'theme_faceup',
-    get_template_directory_uri() . '/css/header.css',
-    array(),
-    '1.0'
-);
-
-}
-add_action( 'wp_enqueue_scripts', 'faceup_register_assets' );
+function faceup_theme() {
+    wp_enqueue_style( 'style', get_template_directory_uri() );
+  }
+add_action( 'wp_enqueue_scripts', 'faceup_theme' );
