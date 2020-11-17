17d8b0d5237645f462534dbc4f086c42eb3baf1b
oscarunit <mateo.pernet@outlook.fr>
2020-11-17 15:34:29 +0100

    add footer.css

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function faceup_theme() {
     wp_enqueue_style( 'style', get_template_directory_uri() );
-    wp_enqueue_style('main_style', get_theme_file_uri('css/header.css'));
+    wp_enqueue_style('header_style', get_theme_file_uri('css/header.css'));
+    wp_enqueue_style('footer_style', get_theme_file_uri('css/footer.css'));
   }
 add_action( 'wp_enqueue_scripts', 'faceup_theme' );
