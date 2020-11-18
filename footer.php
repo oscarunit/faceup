86a53bb5d490583bef9e576d84be759cef5f3df4
Clotilde74 <clotildelarfaillou@gmail.com>
2020-11-18 13:29:18 +0100

    css footer

--- footer.php
+++ footer.php
@@ -1,13 +1,16 @@
 <footer>
 	<div class="container">
-	<div class="info">
+	<div class="informations">
+		<p>Adresse</p>
+		<p>Ville</p>
+		<p>Téléphone</p>
 		<p>clairehermet@hotmail.com</p>
 	</div>
-	<div class="mentionlegale">
-		<a href="https://www.faceup-facialiste.fr/mentionlegale/" id="mentionL">Mentions légales</a>
-		<a href="https://www.instagram.com/faceup_facialist/"><img src="image/instagram.png" id="insta"/></a>
+	<div class="legalmention">
+		<a href="https://www.faceup-facialiste.fr/mentionlegale/"><p>Mentions légales</p></a>
+		<a href="https://www.instagram.com/faceup_facialist/"><img src="<?php echo get_template_directory_uri(); ?>/image/ig.svg" alt="Logo instagram"/></a>
 	</div>
-	<div class="rendezvous">
+	<div class="button">
 		<a href="https://www.instagram.com/prendrerendezvous/" id="BoutonRdv">Prendre rendez-vous</a>
 	</div>
 </footer>
