a11741f02c399096e669799e21465ae57e8d017f
oscarunit <mateo.pernet@outlook.fr>
2020-11-18 17:19:03 +0100

    modif style.css, footer.php, footer.css, functions.php

--- footer.php
+++ footer.php
@@ -1,17 +1,15 @@
 <footer>
 	<div class="container">
-	<div class="informations">
-		<p>Adresse</p>
-		<p>Ville</p>
-		<p>Téléphone</p>
-		<p>clairehermet@hotmail.com</p>
-	</div>
-	<div class="legalmention">
-		<a href="https://www.faceup-facialiste.fr/mentionlegale/"><p>Mentions légales</p></a>
-		<a href="https://www.instagram.com/faceup_facialist/"><img src="<?php echo get_template_directory_uri(); ?>/image/ig.svg" alt="Logo instagram"/></a>
-	</div>
-	<div class="button">
-		<a href="https://www.instagram.com/prendrerendezvous/"<input type="button">Prendre rendez-vous</a>
+		<div class="informations">
+			<p>clairehermet@hotmail.com</p>
+		</div>
+		<div class="legalmention">
+			<a href="https://www.faceup-facialiste.fr/mentionlegale/"><p>Mentions légales</p></a>
+			<a href="https://www.instagram.com/faceup_facialist/"><img src="<?php echo get_template_directory_uri(); ?>/image/ig.svg" alt="Logo instagram"/></a>
+		</div>
+		<div class="button">
+			<a href="https://www.instagram.com/prendrerendezvous/">Prendre rendez-vous</a>
+		</div>
 	</div>
 </footer>
 
--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
 function faceup_theme() {
-    wp_enqueue_style('resetcss', get_theme_file_uri('css/reset.css'));
-    wp_enqueue_style( 'style', get_template_directory_uri() );
-    wp_enqueue_style('header_style', get_theme_file_uri('css/header.css'));
-    wp_enqueue_style('footer_style', get_theme_file_uri('css/footer.css'));
-  }
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style('resetcss', get_theme_file_uri('css/reset.css'));
+  wp_enqueue_style('header_style', get_theme_file_uri('css/header.css'));
+  wp_enqueue_style('footer_style', get_theme_file_uri('css/footer.css'));
+}
 add_action( 'wp_enqueue_scripts', 'faceup_theme' );
 
 register_nav_menus( array(
