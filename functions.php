f1e206d49524e78b51218742dc2dbefaaf0ffd9b
oscarunit <mateo.pernet@outlook.fr>
2020-11-18 20:10:16 +0100

    change functions, footer, page

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function faceup_theme() {
   wp_enqueue_style('resetcss', get_theme_file_uri('css/reset.css'));
   wp_enqueue_style('header_style', get_theme_file_uri('css/header.css'));
   wp_enqueue_style('footer_style', get_theme_file_uri('css/footer.css'));
+  wp_enqueue_style('page_style', get_theme_file_uri('css/page.css'));
 }
 add_action( 'wp_enqueue_scripts', 'faceup_theme' );
 
